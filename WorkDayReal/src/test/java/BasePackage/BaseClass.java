cb034ff8f957ae960201021bd4d1205dc0ba5e41
Vaibhav <Vaibhav@Lenovo-PC>
2020-06-20 13:51:52 +0530

    commmit to hub

---
+++ WorkDayReal/src/test/java/BasePackage/BaseClass.java
@@ -0,0 +1,11 @@
+package BasePackage;
+
+import org.testng.annotations.Test;
+
+public class BaseClass {
+
+	@Test
+	public void Test_01() {
+		System.out.println("Before Changes");
+	}
+}
---
+++ WorkDayReal/src/test/java/BasePackage/BaseClass2.java
@@ -0,0 +1,11 @@
+package BasePackage;
+
+import org.testng.annotations.Test;
+
+public class BaseClass2 {
+
+	@Test
+	public void Test_01() {
+		System.out.println("Before Changes");
+	}
+}
